d76f633b28038fc90a79f931afdbd05f60d85b72
methauros <90660253+methauros@users.noreply.github.com>
2022-02-16 11:39:22 -0300

    Add files via upload

---
+++ calculadora-de-m-dia/calculadora-de-m-dia/src/script.js
@@ -0,0 +1,24 @@
+var nome = "Bruno"
+
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaDoTerceiroBimestre = 4
+var notaDoQuartoBimestre = 2
+
+var notaFinal =
+  (notaDoPrimeiroBimestre +
+    notaDoSegundoBimestre +
+    notaDoTerceiroBimestre +
+    notaDoQuartoBimestre) /
+  4
+
+var notaFixada = notaFinal.toFixed(1)
+
+console.log("Bem vindo " + nome + "!")
+console.log(notaFixada)
+
+if (notaFixada>5) console.log ("Você passou!")
+if (notaFixada<5) console.log ("Está reprovado!")
+
+// Revisão 
+// variáveis, strings, console.log, toFixed, operações matemáticas, concatenação
